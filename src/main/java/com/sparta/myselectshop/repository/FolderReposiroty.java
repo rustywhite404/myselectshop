db7f45269875e619cde60447a42e318a0b4eef18
rustywhite404 <rustywhite404@gmail.com>
2024-11-08 21:40:19 +0900

    폴더 생성 및 조회 구현

--- src/main/java/com/sparta/myselectshop/repository/FolderReposiroty.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.myselectshop.repository;
-
-import com.sparta.myselectshop.entity.Folder;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FolderReposiroty extends JpaRepository<Folder,Long> {
-}
